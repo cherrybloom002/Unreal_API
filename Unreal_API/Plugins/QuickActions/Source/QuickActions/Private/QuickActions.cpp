b7c0627a594e24743517a0e63f5836eec341c26b
cherrybloom002 <161952780+cherrybloom002@users.noreply.github.com>
2025-04-27 16:28:19 +0200

    Correct project + QuickActions Plugin created

---
+++ Unreal_API/Plugins/QuickActions/Source/QuickActions/Private/QuickActions.cpp
@@ -0,0 +1,20 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "QuickActions.h"
+
+#define LOCTEXT_NAMESPACE "FQuickActionsModule"
+
+void FQuickActionsModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+}
+
+void FQuickActionsModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FQuickActionsModule, QuickActions)
